f1a6405b11d02f9cfa909bfc40dfbd3fe54a2aba
Thirumal <m.thirumal@hotmail.com>
2020-03-10 23:15:51 +0530

    updated shell script

--- eureka-server.sh
+++ eureka-server.sh
@@ -1,3 +1,6 @@
 mvn clean package -DskipTests=true
 echo 'starting application'
-java -jar target/eureka-server-0.0.1-SNAPSHOT.jar
+appName="eureka-server-0.0.1-SNAPSHOT.jar"
+echo 'App Name:' $appName
+pkill -f $appName 
+java -jar target/$appName
