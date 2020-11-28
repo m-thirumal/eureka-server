7ebf9d2f86b212cc501631a12a32238b2b68c83d
Thirumal <m.thirumal@hotmail.com>
2020-11-28 14:23:27 +0530

    Upgraded Swagger and cloud M5

--- src/main/java/in/thirumal/config/SwaggerConfig.java
+++ src/main/java/in/thirumal/config/SwaggerConfig.java
@@ -3,13 +3,24 @@
  */
 package in.thirumal.config;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.DocExpansion;
+import springfox.documentation.swagger.web.ModelRendering;
+import springfox.documentation.swagger.web.OperationsSorter;
+import springfox.documentation.swagger.web.TagsSorter;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -18,13 +29,46 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
-//@Profile("!PRO")
+@Profile("!PRO")
 public class SwaggerConfig {
 	
-	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build();
-	}
+	private ApiInfo apiInfo() {
+        return new ApiInfo("Microservice to send notification", "Microservice to send notification via e-mail, sms,..", "API V1", "Terms of service",
+          new Contact("Thirumal", "https://twitter.com/_thirumal", "m.thirumal@hotmail.com"),
+          "License of API", "API license URL", Collections.emptyList());
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
+          .select()
+          .apis(RequestHandlerSelectors.any())
+          .paths(PathSelectors.any())
+          .build();
+    }
+
+    /**
+     * SwaggerUI information
+     */
+
+    @Bean
+    UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()
+          .deepLinking(true)
+          .displayOperationId(false)
+          .defaultModelsExpandDepth(1)
+          .defaultModelExpandDepth(1)
+          .defaultModelRendering(ModelRendering.EXAMPLE)
+          .displayRequestDuration(false)
+          .docExpansion(DocExpansion.NONE)
+          .filter(false)
+          .maxDisplayedTags(null)
+          .operationsSorter(OperationsSorter.ALPHA)
+          .showExtensions(false)
+          .tagsSorter(TagsSorter.ALPHA)
+          .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
+          .validatorUrl(null)
+          .build();
+    }
 	
 }
