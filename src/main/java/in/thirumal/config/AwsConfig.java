d12f615d908eb82feecb1b912ea3a35668fe5ae5
Thirumal <m.thirumal@hotmail.com>
2023-01-17 21:05:19 +0530

    AWS Config

---
+++ src/main/java/in/thirumal/config/AwsConfig.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package in.thirumal.config;
+
+/**
+ * @author Thirumal
+ *
+ */
+//@Configuration
+public class AwsConfig {
+/*
+	@Bean
+	@Profile("!default")
+	public EurekaInstanceConfigBean eurekaInstanceConfig(InetUtils inetUtils) {
+	  EurekaInstanceConfigBean b = new EurekaInstanceConfigBean(inetUtils);
+	  AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+	  b.setDataCenterInfo(info);
+	  return b;
+	}*/
+	
+}
